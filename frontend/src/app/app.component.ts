b285adb6eb8b4b233f14349fac249fc4f6369b62
Joseph <joejoshw@gmail.com>
2019-10-13 17:09:58 +0300

    Added dummy login
    Signed-off-by: Joseph <joejoshw@gmail.com>

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, NavigationStart, NavigationEnd, NavigationError, NavigationCancel, RoutesRecognized } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -11,7 +11,42 @@ export class AppComponent {
   authenticated: Boolean;
   navItems = [];  
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+    router.events.forEach((event) => {
+      if(event instanceof NavigationStart) {
+        console.log("starting nav to " + event.url);
+        var newUrl = event.url;
+        console.log("login index " + newUrl.indexOf("login"))
+        if (newUrl.indexOf("login") > 0 || newUrl.indexOf("register") > 0)  {
+          localStorage.setItem('authenticated', '0');
+        } else
+        {
+          console.log("not going to login");
+          var isAuthenticated = localStorage.getItem('authenticated');
+          console.log("is authenticated = " + isAuthenticated);
+          if (isAuthenticated) {
+            if (isAuthenticated === "1") {
+              // We are authenticated
+              this.authenticated = true;
+            } else {
+              //not authenticated
+              this.authenticated = false;
+              this.router.navigate(['/login']);
+              localStorage.setItem('authenticated', '0');
+            }
+          } else {
+            //Authentication status not set
+            this.router.navigate(['/login']);
+            localStorage.setItem('authenticated', '0');
+          }
+        }
+      }
+      // NavigationEnd
+      // NavigationCancel
+      // NavigationError
+      // RoutesRecognized
+    });
+  }
 
   ngOnInit() {
     this.authenticated = false;
