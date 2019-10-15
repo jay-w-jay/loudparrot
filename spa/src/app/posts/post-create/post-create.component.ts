70694653b98d2d47ae37fbc24e843c6c6aedcb83
Joseph <joejoshw@gmail.com>
2019-10-15 23:50:35 +0300

    Add blank create post component
    Signed-off-by: Joseph <joejoshw@gmail.com>

--- spa/src/app/posts/post-create/post-create.component.ts
+++ spa/src/app/posts/post-create/post-create.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-post-create',
-  templateUrl: 'post-create.component.html'
+  templateUrl: '/post-create.component.html'
 })
 export class PostCreateComponent {
 
